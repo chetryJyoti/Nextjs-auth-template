6fdc6e70e5cbf82ea36bd54fde0ba3a8f8326a37
jyoti chetry <chetryjyoti@jyotis-MacBook-Air.local>
2024-07-03 20:07:40 +0530

    changed text

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
             font.className
           )}
         >
-          Binbag
+          Auth
         </h1>
         <p className="text-white text-lg">
           Your nextjs authentication template
